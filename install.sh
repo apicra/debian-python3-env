ffa8d819fbc62f2f9410655736c39a38abc590b7
tom <tom@sapletta.com>
2020-04-29 16:26:16 +0200

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 python3 -m pip install virtualenv && \
 #python3 venv env && \
-python3 -m venv env && source env/bin/activate && pip install -r src/requirements.txt
+python3 -m venv env
+. env/bin/activate
+pip install -r src/requirements.txt
